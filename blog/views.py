acb41e3d5fb0b191b6e34e61ab0c478de03c97b2
YKNNagi <kyk.main@gmail.com>
2026-08-28 15:45:24 +0900

    Add article creation and dashboard

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Article, Tag
+from .forms import ArticleForm
 
-# Create your views here.
+def create(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard")
+
+    else:
+        form = ArticleForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "blog/article_form.html", context)
+
+def dashboard(request):
+    articles = Article.objects.all()
+
+    context = {
+        "articles": articles
+    }
+
+    return render(request, "blog/dashboard.html", context)
